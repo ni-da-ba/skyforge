50412d5d57d2c609f7b3b442dc0598919c7bde5e
Nicholas Babineaux <142941255+ni-da-ba@users.noreply.github.com>
2026-08-31 11:18:43 -0500

    SF-IMP-0031: add NeoForge adapter module and centralized repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,13 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        maven("https://maven.neoforged.net/releases")
         mavenCentral()
     }
+}
+
+plugins {
+    id("net.neoforged.moddev.repositories") version "2.0.144"
 }
 
 dependencyResolutionManagement {
@@ -20,4 +25,5 @@ include(
     "skyforge-recipes",
     "skyforge-world",
     "skyforge-reference",
+    "skyforge-neoforge-1211",
 )
