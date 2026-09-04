70d88e27cc7b8ce37a83763c744a896c8f333ae0
Nicholas Babineaux <142941255+ni-da-ba@users.noreply.github.com>
2026-09-03 22:01:12 -0500

    SF-IMP-0062 register stacked decoration proof

--- skyforge-neoforge-1211/src/main/java/io/github/nidaba/skyforge/neoforge1211/SkyforgeNeoForge1211Mod.java
+++ skyforge-neoforge-1211/src/main/java/io/github/nidaba/skyforge/neoforge1211/SkyforgeNeoForge1211Mod.java
@@ -29,6 +29,7 @@ public final class SkyforgeNeoForge1211Mod {
         SkyforgeNeoForge1211LocalModificationsDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211CarverDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211UndergroundDecorationDevRuntime.installFromSystemProperty();
+        SkyforgeNeoForge1211UndergroundDecorationStackedDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211CarverReloadDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211CarverStackedDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211LocalModificationsStackedDevRuntime.installFromSystemProperty();
