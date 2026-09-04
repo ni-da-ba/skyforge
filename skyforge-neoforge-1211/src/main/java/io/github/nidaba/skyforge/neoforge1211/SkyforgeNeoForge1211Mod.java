ed65ecb144a392a4fc0c69b3a330145b2852dcde
Nicholas Babineaux <142941255+ni-da-ba@users.noreply.github.com>
2026-09-03 20:22:32 -0500

    SF-IMP-0061: install reload and stacked proof modes

--- skyforge-neoforge-1211/src/main/java/io/github/nidaba/skyforge/neoforge1211/SkyforgeNeoForge1211Mod.java
+++ skyforge-neoforge-1211/src/main/java/io/github/nidaba/skyforge/neoforge1211/SkyforgeNeoForge1211Mod.java
@@ -28,6 +28,8 @@ public final class SkyforgeNeoForge1211Mod {
         SkyforgeNeoForge1211PhysicalAdmissionDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211LocalModificationsDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211CarverDevRuntime.installFromSystemProperty();
+        SkyforgeNeoForge1211CarverReloadDevRuntime.installFromSystemProperty();
+        SkyforgeNeoForge1211CarverStackedDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211LocalModificationsStackedDevRuntime.installFromSystemProperty();
         SkyforgeNeoForge1211UndergroundStackedDevRuntime.installFromSystemProperty();
     }
