1b6225ab8b6d44880f4c46bd6bc256520533b3b1
Nicholas Babineaux <142941255+ni-da-ba@users.noreply.github.com>
2026-09-25 17:24:49 -0500

    auth: add F1 qualified fluvial realization

---
+++ skyforge-world/src/main/java/io/github/nidaba/skyforge/world/SkyIslandQualifiedFluvialZone.java
@@ -0,0 +1,9 @@
+package io.github.nidaba.skyforge.world;
+
+/** Local realization zone for one accepted fluvial influence. */
+public enum SkyIslandQualifiedFluvialZone {
+    UNAFFECTED,
+    CHANNEL_BED,
+    BANKFULL_CORRIDOR,
+    VALLEY_RECOVERY
+}
