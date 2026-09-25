8b438d70453855db9dd55fd8691a6c5d3f8bb10b
Nicholas Babineaux <142941255+ni-da-ba@users.noreply.github.com>
2026-09-25 16:52:08 -0500

    auth: restack E2 on clean E1

---
+++ skyforge-world/src/main/java/io/github/nidaba/skyforge/world/SkyIslandContinuousWaterbodyQualificationViolation.java
@@ -0,0 +1,10 @@
+package io.github.nidaba.skyforge.world;
+
+/** Objective pre-authoring retained-basin qualification failures. */
+public enum SkyIslandContinuousWaterbodyQualificationViolation {
+    SEARCH_BOUNDARY_ESCAPE,
+    MISSING_SHORELINE,
+    EXCESSIVE_DEPTH_TO_DIAMETER,
+    EXCESSIVE_SHORELINE_GRADE,
+    INSUFFICIENT_SPILL_HEADROOM
+}
